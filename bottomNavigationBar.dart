e3a9ec639464eef77d9861f34c7305d3b036feec
abperky <69203638+abperky@users.noreply.github.com>
2020-12-01 17:10:29 +0600

    Create bottomNavigationBar.dart

---
+++ bottomNavigationBar.dart
@@ -0,0 +1,12 @@
+bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.yellow,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ("Item")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit_rounded), label: ("Item")),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ("Item")),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ("Item")),
+          BottomNavigationBarItem(icon: Icon(Icons.ac_unit), label: ("Item")),
+        ],
+      ),
